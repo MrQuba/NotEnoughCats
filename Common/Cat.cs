29ca612be11c1af3b1880260cfe660fca236096e
MrQuba <mrquba4@gmail.com>
2024-05-27 09:15:56 +0200

    added Cat
    added base class for pet projectiles, to simplify adding more in the future

---
+++ Common/Cat.cs
@@ -0,0 +1,104 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using NotEnoughCats.Content.Buff;
+
+namespace NotEnoughCats.Common
+{
+	// base class for cats
+	public class Cat
+	{
+		// sets static defaults and preview animation
+		public void SetStaticDefaults(Projectile Projectile)
+		{
+			Main.projFrames[Projectile.type] = 4;
+			Main.projPet[Projectile.type] = true;
+			ProjectileID.Sets.CharacterPreviewAnimations[Projectile.type] = ProjectileID.Sets.SimpleLoop(0, Main.projFrames[Projectile.type], 12)
+				.WithOffset(-10, 0)
+				.WithSpriteDirection(-1)
+				.WithCode(DelegateMethods.CharacterPreview.Float);
+		}
+		// sets defaults for pet
+		public void SetDefaults(Projectile Projectile, int AIType)
+		{
+			Projectile.CloneDefaults(ProjectileID.BabyFaceMonster);
+			Projectile.height = 28;
+			Projectile.width = 34;
+			AIType = -1;
+			Main.projFrames[Projectile.type] = 4;
+		}
+		// AI of pet
+		public void AI(Projectile Projectile)
+		{
+			Player player = Main.player[Projectile.owner];
+			int frameSpeed = 25;
+			gravityForce(Projectile);
+			Animate(Projectile, player, frameSpeed);
+			isActive(Projectile, player, ModContent.BuffType<BlackCatBuff>());
+		}
+		// checks if there is solid tile below projectile
+		private bool isSolidTileBelow(Projectile Projectile)
+		{
+			bool isSolidTileBelow = false;
+			int tileX = (int)(Projectile.position.X / 16);
+			int tileY = (int)((Projectile.position.Y + Projectile.height) / 16);
+
+			if (WorldGen.SolidTile(tileX, tileY))
+			{
+				isSolidTileBelow = true;
+			}
+			return isSolidTileBelow;
+		}
+		// applies gravity force if projectile is floating
+		private void gravityForce(Projectile Projectile)
+		{
+			Player player = Main.player[Projectile.owner];
+			if (!isSolidTileBelow(Projectile) && !isPlayerMovingOnYAxis(player))
+			{
+				Projectile.velocity.Y += 0.981f;
+			}
+		}
+		// checks if pet is active
+		private bool isActive(Projectile Projectile, Player player, int buffType)
+		{
+			if (!player.dead && player.HasBuff(buffType))
+			{
+				Projectile.timeLeft = 2;
+				return true;
+
+			}
+			return false;
+		}
+		// Plays animation if player is moving
+		private bool isPlayerMovingOnXAxis(Player player)
+		{
+			return player.velocity.X != 0;
+		}
+		private bool isPlayerMovingOnYAxis(Player player)
+		{
+			return player.velocity.Y > 0;
+		}
+		private void Animate(Projectile Projectile, Player player, int frameSpeed)
+		{
+
+			if (!isPlayerMovingOnXAxis(player))
+			{
+				Projectile.frame = 0;
+			}
+			else
+			{
+				Projectile.frameCounter++;
+				if (Projectile.frameCounter >= frameSpeed)
+				{
+					Projectile.frameCounter = 0;
+					Projectile.frame++;
+					if (Projectile.frame >= Main.projFrames[Projectile.type])
+					{
+						Projectile.frame = 0;
+					}
+				}
+			}
+		}
+	}
+}
+
--- Content/Pets/BlackCatProjectile.cs
+++ Content/Pets/BlackCatProjectile.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
-using NotEnoughCats.Content.Buff;
+﻿using NotEnoughCats.Content.Buff;
+using NotEnoughCats.Common;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -8,48 +8,21 @@ namespace NotEnoughCats.Content.Pets
 {
 	public class BlackCatProjectile : ModProjectile
 	{
+		private readonly Cat blackCat = new();
+		// sets static defaults and preview animation
 		public override void SetStaticDefaults()
 		{
-			Main.projFrames[Projectile.type] = 4;
-			Main.projPet[Projectile.type] = true;
-			ProjectileID.Sets.CharacterPreviewAnimations[Projectile.type] = ProjectileID.Sets.SimpleLoop(0, Main.projFrames[Projectile.type], 12)
-				.WithOffset(-10, 0)
-				.WithSpriteDirection(-1)
-				.WithCode(DelegateMethods.CharacterPreview.Float);
+			blackCat.SetStaticDefaults(Projectile);
 		}
-
+		// sets defaults for pet
 		public override void SetDefaults()
 		{
-			Projectile.CloneDefaults(ProjectileID.BabyRedPanda);
-			Projectile.height = 30;
-			Projectile.width = 44;
-			AIType = ProjectileID.BabyRedPanda;
+			blackCat.SetDefaults(Projectile, AIType);
 		}
+		// AI of pet
 		public override void AI()
 		{
-			Player player = Main.player[Projectile.owner];
-			int frameSpeed = 30;
-			if(player.velocity.X == 0)
-			{
-				Projectile.frame = 0;
-			}
-			else{
-				Projectile.frameCounter++;
-				if (Projectile.frameCounter >= frameSpeed)
-				{
-					Projectile.frameCounter = 0;
-					Projectile.frame++;
-					if (Projectile.frame >= Main.projFrames[Projectile.type] - 1)
-					{
-						Projectile.frame = 0;
-					}
-				}
-			}
-
-			if (!player.dead && player.HasBuff(ModContent.BuffType<BlackCatBuff>()))
-			{
-				Projectile.timeLeft = 2;
-			}
+			blackCat.AI(Projectile);
 		}
 	}
 }
